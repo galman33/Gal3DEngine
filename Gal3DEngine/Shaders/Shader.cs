4c4c329aff6f1a005e1a3652cf691f7d681a8566
Gal Pasternak <galman33@gmail.com>
2014-12-07 15:44:26 +0200

    Extracted generic methods into Shader

--- Gal3DEngine/Shaders/Shader.cs
+++ Gal3DEngine/Shaders/Shader.cs
@@ -3,118 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using OpenTK;
+using Gal3DEngine.IndicesTypes;
 
 namespace Gal3DEngine
 {
     class Shader
     {
 
-        public static Matrix4 world;
-        public static Matrix4 view;
-        public static Matrix4 projection;
+        public delegate void TransformMethod<InData, InTransformation>(ref InData data, InTransformation transformation, Screen screen);
+        public delegate OutLineData ProcessScanLineMethod<OutLineData, InIndex>(float gradient1, float gradient2, InIndex pa, InIndex pb, InIndex pc, InIndex pd);
+        public delegate void ProcessPixelMethod<InLineData>(int x, int y, float gradient, ref InLineData lineData, Screen screen);
 
-        public static Color3 Color;
+        protected static Vector4[] positions;
 
-        public static void Render(Screen screen, VertexColor[] vertices, int[] indices)
+        public static void SetVerticesPositions(Vector4[] positions)
         {
-            Matrix4 transformation = world * view * projection;
-
-            VertexColor[] transformedVertices = new VertexColor[vertices.Length];
-
-            Vector4 v;
-            int i;
-
-            for (i = 0; i < vertices.Length; i++)
-            {
-                v = Vector4.Transform(vertices[i].Position, view * world * projection); // projection * view * world
-
-                v.X = v.X / v.W * 0.5f * screen.Width + screen.Width / 2;
-                v.Y = v.Y / v.W * 0.5f * screen.Height + screen.Height / 2;
-                v.Z = v.Z / v.W;
-
-                transformedVertices[i].Position = v;
-                transformedVertices[i].Color = vertices[i].Color;
-            }
-
-            for (i = 0; i < indices.Length; i += 3)
-            {
-                if (!Shader.ShouldCull(transformedVertices[indices[i + 0]].Position, transformedVertices[indices[i + 1]].Position, transformedVertices[indices[i + 2]].Position))
-                    DrawTriangle(screen, transformedVertices[indices[i + 0]], transformedVertices[indices[i + 1]], transformedVertices[indices[i + 2]], Color);
-            }
-            
-
+            ShaderPhong.positions = (Vector4[])positions.Clone();
         }
 
-        private static float Lerp(float a, float b, float t)
+        protected static void TransformData<InData, InTransformation>(TransformMethod<InData, InTransformation> transformMethod, InData[] data, InTransformation transformation, Screen screen)
         {
-            return a + (b - a) * t;
-        }
-
-        private static Color3 ColorLerp(Color3 a, Color3 b, float t)
-        {
-            Color3 result;
-            result.r = (byte)(a.r + (b.r - a.r) * t);
-            result.g = (byte)(a.g + (b.g - a.g) * t);
-            result.b = (byte)(a.b + (b.b - a.b) * t);
-            return result;
-        }
-
-        // drawing line between 2 points from left to right
-        // papb -> pcpd
-        // pa, pb, pc, pd must then be sorted before
-        private static void ProcessScanLine(Screen screen, int y, VertexColor pa, VertexColor pb, VertexColor pc, VertexColor pd, Color3 color)
-        {
-            // Thanks to current Y, we can compute the gradient to compute others values like
-            // the starting X (sx) and ending X (ex) to draw between
-            // if pa.Y == pb.Y or pc.Y == pd.Y, gradient is forced to 1
-            var gradient1 = pa.Position.Y != pb.Position.Y ? (y - pa.Position.Y) / (pb.Position.Y - pa.Position.Y) : 1;
-            var gradient2 = pc.Position.Y != pd.Position.Y ? (y - pc.Position.Y) / (pd.Position.Y - pc.Position.Y) : 1;
-
-            int sx = (int)Lerp(pa.Position.X, pb.Position.X, gradient1);
-            int ex = (int)Lerp(pc.Position.X, pd.Position.X, gradient2);
-
-            // starting Z & ending Z
-            float z1 = Lerp(pa.Position.Z, pb.Position.Z, gradient1);
-            float z2 = Lerp(pc.Position.Z, pd.Position.Z, gradient2);
-
-            // starting Z & ending Z
-            Color3 c1 = ColorLerp(pa.Color, pb.Color, gradient1);
-            Color3 c2 = ColorLerp(pc.Color, pd.Color, gradient2);
-
-
-
-            // drawing a line from left (sx) to right (ex) 
-            for (var x = sx; x < ex; x++)
+            for (int i = 0; i < data.Length; i++)
             {
-                float gradient = (x - sx) / (float)(ex - sx);
-
-                var z = Lerp(z1, z2, gradient);
-                Color3 c = ColorLerp(c1, c2, gradient);
-
-                screen.TryPutPixel(x, y, z, c);
+                transformMethod(ref data[i], transformation, screen);
             }
         }
 
-        private static void DrawTriangle(Screen screen, VertexColor p1, VertexColor p2, VertexColor p3, Color3 color)
+        protected static void DrawTriangles<InIndex, InLineData>(InIndex[] indices, Screen screen, ProcessScanLineMethod<InLineData, InIndex> processScanLine, ProcessPixelMethod<InLineData> processPixel) where InIndex : IndexPosition
+        {
+            for (int i = 0; i < indices.Length; i += 3)
+            {
+                if (Shader.ShouldRender(positions[indices[i + 0].position], positions[indices[i + 1].position], positions[indices[i + 2].position], screen.Width, screen.Height))
+                {
+                    DrawTriangle(screen, indices[i + 0], indices[i + 1], indices[i + 2],
+                        processScanLine, processPixel);
+                }
+            }
+        }
+
+        private static void DrawTriangle<InIndex, InLineData>(Screen screen, InIndex p1, InIndex p2, InIndex p3, ProcessScanLineMethod<InLineData, InIndex> processScanLine, ProcessPixelMethod<InLineData> processPixel) where InIndex : IndexPosition
         {
             // Sorting the points in order to always have this order on screen p1, p2 & p3
             // with p1 always up (thus having the Y the lowest possible to be near the top screen)
             // then p2 between p1 & p3
-            if (p1.Position.Y > p2.Position.Y)
+            if (positions[p1.position].Y > positions[p2.position].Y)
             {
                 var temp = p2;
                 p2 = p1;
                 p1 = temp;
             }
 
-            if (p2.Position.Y > p3.Position.Y)
+            if (positions[p2.position].Y > positions[p3.position].Y)
             {
                 var temp = p2;
                 p2 = p3;
                 p3 = temp;
             }
 
-            if (p1.Position.Y > p2.Position.Y)
+            if (positions[p1.position].Y > positions[p2.position].Y)
             {
                 var temp = p2;
                 p2 = p1;
@@ -126,13 +72,13 @@ namespace Gal3DEngine
 
             // http://en.wikipedia.org/wiki/Slope
             // Computing inverse slopes
-            if (p2.Position.Y - p1.Position.Y > 0)
-                dP1P2 = (p2.Position.X - p1.Position.X) / (p2.Position.Y - p1.Position.Y);
+            if (positions[p2.position].Y - positions[p1.position].Y > 0)
+                dP1P2 = (positions[p2.position].X - positions[p1.position].X) / (positions[p2.position].Y - positions[p1.position].Y);
             else
                 dP1P2 = 0;
 
-            if (p3.Position.Y - p1.Position.Y > 0)
-                dP1P3 = (p3.Position.X - p1.Position.X) / (p3.Position.Y - p1.Position.Y);
+            if (positions[p3.position].Y - positions[p1.position].Y > 0)
+                dP1P3 = (positions[p3.position].X - positions[p1.position].X) / (positions[p3.position].Y - positions[p1.position].Y);
             else
                 dP1P3 = 0;
 
@@ -149,15 +95,15 @@ namespace Gal3DEngine
             // P3
             if (dP1P2 > dP1P3)
             {
-                for (var y = (int)p1.Position.Y; y <= (int)p3.Position.Y; y++)
+                for (var y = (int)positions[p1.position].Y; y <= (int)positions[p3.position].Y; y++)
                 {
-                    if (y < p2.Position.Y)
+                    if (y < positions[p2.position].Y)
                     {
-                        ProcessScanLine(screen, y, p1, p3, p1, p2, color);
+                        ProcessScanLine(screen, y, p1, p3, p1, p2, processScanLine, processPixel);
                     }
                     else
                     {
-                        ProcessScanLine(screen, y, p1, p3, p2, p3, color);
+                        ProcessScanLine(screen, y, p1, p3, p2, p3, processScanLine, processPixel);
                     }
                 }
             }
@@ -174,17 +120,42 @@ namespace Gal3DEngine
             //       P3
             else
             {
-                for (var y = (int)p1.Position.Y; y <= (int)p3.Position.Y; y++)
+                for (var y = (int)positions[p1.position].Y; y <= (int)positions[p3.position].Y; y++)
                 {
-                    if (y < p2.Position.Y)
+                    if (y < positions[p2.position].Y)
                     {
-                        ProcessScanLine(screen, y, p1, p2, p1, p3, color);
+                        ProcessScanLine(screen, y, p1, p2, p1, p3, processScanLine, processPixel);
                     }
                     else
                     {
-                        ProcessScanLine(screen, y, p2, p3, p1, p3, color);
+                        ProcessScanLine(screen, y, p2, p3, p1, p3, processScanLine, processPixel);
                     }
                 }
+            }
+        }
+
+        // drawing line between 2 points from left to right
+        // papb -> pcpd
+        // pa, pb, pc, pd must then be sorted before
+        private static void ProcessScanLine<InIndex, InLineData>(Screen screen, int y, InIndex pa, InIndex pb, InIndex pc, InIndex pd, ProcessScanLineMethod<InLineData, InIndex> processScanLine, ProcessPixelMethod<InLineData> processPixel) where InIndex : IndexPosition
+        {
+            // Thanks to current Y, we can compute the gradient to compute others values like
+            // the starting X (sx) and ending X (ex) to draw between
+            // if pa.Y == pb.Y or pc.Y == pd.Y, gradient is forced to 1
+            var gradient1 = positions[pa.position].Y != positions[pb.position].Y ? (y - positions[pa.position].Y) / (positions[pb.position].Y - positions[pa.position].Y) : 1;
+            var gradient2 = positions[pc.position].Y != positions[pd.position].Y ? (y - positions[pc.position].Y) / (positions[pd.position].Y - positions[pc.position].Y) : 1;
+
+            int sx = (int)Lerp(positions[pa.position].X, positions[pb.position].X, gradient1);
+            int ex = (int)Lerp(positions[pc.position].X, positions[pd.position].X, gradient2);
+
+            InLineData lineData = processScanLine(gradient1, gradient2, pa, pb, pc, pd);
+
+            // drawing a line from left (sx) to right (ex) 
+            for (var x = sx; x < ex; x++)
+            {
+                float gradient = (x - sx) / (float)(ex - sx);
+
+                processPixel(x, y, gradient, ref lineData, screen);
             }
         }
 
@@ -202,9 +173,34 @@ namespace Gal3DEngine
         // Clipping check
         private static bool ShouldClip(Vector4 p1, Vector4 p2, Vector4 p3, int width, int height)
         {
-            return  ((p1.X < 0 || p1.X > width) || (p1.Y < 0 || p1.Y > height) || p1.Z < 0) &&
-                    ((p2.X < 0 || p2.X > width) || (p2.Y < 0 || p2.Y > height) || p2.Z < 0) &&
-                    ((p3.X < 0 || p3.X > width) || (p3.Y < 0 || p3.Y > height) || p3.Z < 0);
+            // W??
+            return  ((p1.X < 0 || p1.X > width) || (p1.Y < 0 || p1.Y > height) || p1.W > 0) &&
+                    ((p2.X < 0 || p2.X > width) || (p2.Y < 0 || p2.Y > height) || p2.W > 0) &&
+                    ((p3.X < 0 || p3.X > width) || (p3.Y < 0 || p3.Y > height) || p3.W > 0);
+        }
+
+        protected static float Lerp(float a, float b, float t)
+        {
+            return a + (b - a) * t;
+        }
+
+        protected static Vector2 Lerp(Vector2 a, Vector2 b, float t)
+        {
+            return new Vector2(a.X + (b.X - a.X) * t, a.Y + (b.Y - a.Y) * t);
+        }
+
+        protected static Vector3 Lerp(Vector3 a, Vector3 b, float t)
+        {
+            return new Vector3(a.X + (b.X - a.X) * t, a.Y + (b.Y - a.Y) * t, a.Z + (b.Z - a.Z) * t);
+        }
+
+        protected static Color3 ColorLerp(Color3 a, Color3 b, float t)
+        {
+            Color3 result;
+            result.r = (byte)(a.r + (b.r - a.r) * t);
+            result.g = (byte)(a.g + (b.g - a.g) * t);
+            result.b = (byte)(a.b + (b.b - a.b) * t);
+            return result;
         }
 
     }
