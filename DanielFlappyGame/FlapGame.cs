02a216090df50cdaede549843dbdeab654ef532b
DanielIndie <danielindie95@gmail.com>
2014-12-28 14:20:39 +0200

    INITIAL COMMIT FUCK

---
+++ DanielFlappyGame/FlapGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gal3DEngine;
+
+namespace DanielFlappyGame
+{
+    public class FlapGame : Game
+    {
+        public FlapGame() : base(640, 480)
+        {
+
+        }
+    }
+}
