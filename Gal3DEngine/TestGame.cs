bc22c689d494eabf1ee1480043066fc452eee81b
Gal Pasternak <galman33@gmail.com>
2015-01-19 09:46:07 +0200

    Testing wrong texture uv interpolation

--- Gal3DEngine/TestGame.cs
+++ Gal3DEngine/TestGame.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using OpenTK;
 using Gal3DEngine.Gizmos;
+using Gal3DEngine.IndicesTypes;
 
 namespace Gal3DEngine
 {
@@ -13,6 +14,8 @@ namespace Gal3DEngine
         private List<Model> models = new List<Model>();
 
         Random rand = new Random();
+
+        Color3[,] tex;
 
         private Camera cam;
 
@@ -25,10 +28,44 @@ namespace Gal3DEngine
         {
             base.OnLoad(e);
             cam = new Camera();
-            for (int i = 0; i < 2; i++)
+            cam.Position.Z = 3;
+            tex = Texture.LoadTexture("Resources/Cat2.png");
+            /*for (int i = 0; i < 2; i++)
             {
                 models.Add(new Model("Resources/Cat2.obj", "Resources/Cat2.png"));
-            }
+            }*/
+            SetUp();
+        }
+
+        Vector4[] positions;
+        Vector2[] uvs;
+        Vector3[] Normal;
+        IndexPositionUVNormal[] indices;
+
+        private void SetUp()
+        {
+            positions = new Vector4[4];
+            positions[0] = new Vector4(1, 1, 0, 1);
+            positions[1] = new Vector4(1, 0, 0, 1);
+            positions[2] = new Vector4(0, 0, 0, 1);
+            positions[3] = new Vector4(0, 1, 0, 1);
+
+            uvs = new Vector2[4];
+            uvs[0] = new Vector2(1, 1);
+            uvs[1] = new Vector2(1, 0);
+            uvs[2] = new Vector2(0, 0);
+            uvs[3] = new Vector2(0, 1);
+
+            Normal = new Vector3[] { new Vector3(0, 0, 1) };
+
+            indices = new IndexPositionUVNormal[6];
+            indices[0] = new IndexPositionUVNormal(0, 0, 0);
+            indices[1] = new IndexPositionUVNormal(3, 3, 0);
+            indices[2] = new IndexPositionUVNormal(2, 2, 0);
+
+            indices[3] = new IndexPositionUVNormal(1, 1, 0);
+            indices[4] = new IndexPositionUVNormal(0, 0, 0);
+            indices[5] = new IndexPositionUVNormal(2, 2, 0);
         }
 
         protected override void OnMouseWheel(OpenTK.Input.MouseWheelEventArgs e)
@@ -70,9 +107,22 @@ namespace Gal3DEngine
             Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)Width / (float)Height, 0.1f, 10.0f);
             Matrix4 view = cam.GetViewMatrix();
 
-            DrawModel(projection, view);
+            //DrawModel(projection, view);
 
-            AxisGizmo.Render(Screen, Matrix4.Identity, view, projection);
+            //AxisGizmo.Render(Screen, Matrix4.Identity, view, projection);
+
+            AvailableShaders.ShaderFlat.projection = projection;
+            AvailableShaders.ShaderFlat.view = view;
+            AvailableShaders.ShaderFlat.lightDirection = Vector3.Normalize(new Vector3(0, 0, -1));
+
+            AvailableShaders.ShaderFlat.texture = tex;
+            AvailableShaders.ShaderFlat.world = Matrix4.CreateRotationY((float) Math.Sin(Time.TotalTime * 3));
+            //AvailableShaders.ShaderFlat.world = Matrix4.Identity;
+            AvailableShaders.ShaderFlat.SetVerticesPositions(positions);
+            AvailableShaders.ShaderFlat.SetVerticesUvs(uvs);
+            AvailableShaders.ShaderFlat.SetVerticesNormals(Normal);
+            AvailableShaders.ShaderFlat.SetIndices(indices);
+            AvailableShaders.ShaderFlat.Render(Screen);
         }
 
         private void DrawModel(Matrix4 projection, Matrix4 view)
@@ -80,7 +130,7 @@ namespace Gal3DEngine
             AvailableShaders.ShaderFlat.projection = projection;
             AvailableShaders.ShaderFlat.view = view;
 
-            AvailableShaders.ShaderFlat.lightDirection = Vector3.Normalize(new Vector3(0, 0, 1));
+            AvailableShaders.ShaderFlat.lightDirection = Vector3.Normalize(new Vector3(0, 0, -1));
 
             for (int i = 0; i < 1; i++)
             {
