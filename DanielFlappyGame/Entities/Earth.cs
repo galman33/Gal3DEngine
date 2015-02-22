32846397aedcbb98b4ab50d3f40c5d7b107918ef
DanielIndie95 <danielindie95@gmail.com>
2015-02-22 20:23:50 +0200

    try some floor logic

---
+++ DanielFlappyGame/Entities/Earth.cs
@@ -0,0 +1,28 @@
+﻿using Gal3DEngine;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DanielFlappyGame.Entities
+{
+    public class Earth : Entity
+    {
+
+        public Earth(Vector3 translation, Vector3 rotation, Vector3 scale, Model model) : base(translation , rotation , scale , model)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if(Math.Abs(Program.world as FlapGame).flappyflappy.Position.Z- this.Position.Z > 10)
+            {
+                this.Position.Z += 10;
+            }
+        }
+
+    }
+}
