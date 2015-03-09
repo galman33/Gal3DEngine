077ad0c607966ea611ee6ed50b35477e596b05a7
DanielIndie95 <danielindie95@gmail.com>
2015-03-09 10:59:13 +0200

    Added highscore interface

---
+++ DanielFlappyGame/HighScoresManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace DanielFlappyGame
+{
+    public class HighScoresManager
+    {
+        XDocument doc;
+        List<Score> scores;
+        public HighScoresManager()
+        {
+            
+        }
+
+        private void LoadHighScores()
+        {
+            doc = XDocument.Load("HighScores.xml");
+            
+        }
+
+        public List<Score> GetScores()
+        {
+            return null;
+        }
+
+        public void AddScore(Score score)
+        {
+            
+        }
+
+        public Score GetTopScore()
+        {
+            List<Score> scores = GetScores();
+            int max = 0;
+            Score maxScore = null;
+
+            foreach(Score score in scores)
+            {
+                if(score.points > max)
+                {
+                    max = score.points;
+                    maxScore = score;
+                }
+            }
+            return maxScore;
+        }
+
+    }
+
+    public class Score
+    {
+        public int points;
+        public string name;
+        public DateTime date;
+
+        public string MakeScoreElement()
+        {           
+           return  "<Score>" + "<Points>" + points + "</Points>" + "<Name>" + points + "</Name>" + "<Date>" + points + "</Date> </Scores>";
+        }
+    }
+}
