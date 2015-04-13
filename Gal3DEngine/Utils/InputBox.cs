c76adfc438b8d4f8d64516fe1004e817088d2c3a
Gal Pasternak <galman33@gmail.com>
2015-04-13 09:52:02 +0300

    Added InputBox to engine

---
+++ Gal3DEngine/Utils/InputBox.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gal3DEngine.Utils
+{
+	class InputBox
+	{
+
+		public static string InputBox(string prompt, string title = "", string defaultResponse = "", int xPos = -1, int yPos = -1)
+		{
+			return Interaction.InputBox(prompt, title, defaultResponse, xPos, yPos);
+		}
+
+	}
+}
