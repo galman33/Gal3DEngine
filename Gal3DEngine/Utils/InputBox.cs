a4a10e98433cbc0fddc08ae3b6bf27ddee605325
DanielIndie95 <danielindie95@gmail.com>
2015-06-07 21:37:25 +0300

    Add documentation to InputBox

--- Gal3DEngine/Utils/InputBox.cs
+++ Gal3DEngine/Utils/InputBox.cs
@@ -6,9 +6,20 @@ using System.Text;
 
 namespace Gal3DEngine.Utils
 {
+    /// <summary>
+    /// Helper class providing an input box for the user.
+    /// </summary>
 	public static class InputBox
 	{
-
+        /// <summary>
+        /// Show an input box with a given - default text, title , position.
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <param name="title">The Title of the input box.</param>
+        /// <param name="defaultResponse">The default response of the input box.</param>
+        /// <param name="xPos">The X cordinate of the input box.</param>
+        /// <param name="yPos">The Y cordinate of the input box.</param>
+        /// <returns></returns>
 		public static string Show(string prompt, string title = "", string defaultResponse = "", int xPos = -1, int yPos = -1)
 		{
 			return Interaction.InputBox(prompt, title, defaultResponse, xPos, yPos);
