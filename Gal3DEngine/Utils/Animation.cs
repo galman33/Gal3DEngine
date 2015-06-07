abcefcdb661023654b77a638012f5b4b6dc1153f
DanielIndie95 <danielindie95@gmail.com>
2015-06-07 13:14:49 +0300

    Added documentation to Animation

--- Gal3DEngine/Utils/Animation.cs
+++ Gal3DEngine/Utils/Animation.cs
@@ -5,16 +5,45 @@ using System.Text;
 
 namespace Gal3DEngine
 {
+    /// <summary>
+    /// Responsible for animation logic.
+    /// </summary>
     public class Animation
     {
+        /// <summary>
+        /// Holds the data of the animations.
+        /// </summary>
         private List<AnimationData> animations;
 
+        /// <summary>
+        /// The current frame of the current playing animation.
+        /// </summary>
         public Model frameModel; 
+        /// <summary>
+        /// The index of the current frame in the current playing animation.
+        /// </summary>
         private int index;
+        /// <summary>
+        /// Should the animation run in the current frame.
+        /// </summary>
         private bool play;
+        /// <summary>
+        /// The current playing animation
+        /// </summary>
         private string curAnimName;
+        /// <summary>
+        /// Time passed since the start of the current frame
+        /// </summary>
         private double curTimePassed;
 
+        /// <summary>
+        /// Initiallize an animation from given models , animation name, frame rate, and if repeat.
+        /// </summary>
+        /// <param name="frameModel">The current frame of the model.</param>
+        /// <param name="models">The models of the animation.</param>
+        /// <param name="animName">The animation name.</param>
+        /// <param name="framesRate">The animation frame rate.</param>
+        /// <param name="repeat">Should the animation repeat itself once done.</param>
         public Animation(Model frameModel , Model[] models, string animName, int framesRate, bool repeat)
         {
             animations = new List<AnimationData>();
@@ -28,17 +57,29 @@ namespace Gal3DEngine
             this.frameModel = entityModel;           
         }
 
+        
+        /// <summary>
+        /// Activates an animation
+        /// </summary>
+        /// <param name="animName">The animation name to play</param>
         public void Play(string animName)
         {
             play = true;
             curAnimName = animName;
         }
 
+        /// <summary>
+        /// Stops the current animation.
+        /// </summary>
         public void Stop()
         {
             play = false;
         }
 
+
+        /// <summary>
+        /// Update the animation (every frame of the engine)
+        /// </summary>
         public void Update()
         {
             if (play)
@@ -67,6 +108,7 @@ namespace Gal3DEngine
             }
         }
 
+
         private bool IsAnimationExists(string name)
         {
             return  (animations.Count<AnimationData>(n => n.AnimationName == curAnimName) > 0);
@@ -80,16 +122,38 @@ namespace Gal3DEngine
             return null;
         }      
 
+        /// <summary>
+        /// Adds a new animation.
+        /// </summary>
+        /// <param name="animName">The new animation name.</param>
+        /// <param name="animationModels">The new animation frames(models).</param>
+        /// <param name="frameRate">The new animation frame rate.</param>
+        /// <param name="repeat">If the new animation should repeat itself once done.</param>
         public void AddAnimation(string animName, Model[] animationModels, int frameRate, bool repeat)
         {
             animations.Add(new AnimationData() { frameRate = frameRate, AnimationName = animName, animationFrames = animationModels, repeat = repeat });
         }
 
+        /// <summary>
+        /// Holds the animation data.
+        /// </summary>
         class AnimationData
         {
+            /// <summary>
+            /// The animation frame rate.
+            /// </summary>
             public int frameRate;
+            /// <summary>
+            /// If The animation should repeat itself one done.
+            /// </summary>
             public bool repeat;
+            /// <summary>
+            /// The animation frames.
+            /// </summary>
             public Model[] animationFrames;
+            /// <summary>
+            /// The animation name.
+            /// </summary>
             public string AnimationName;
         }
 
