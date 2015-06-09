2b74f7bff3409fc3639ceb5f9628e667a1e982b5
DanielIndie95 <danielindie95@gmail.com>
2015-06-09 22:06:31 +0300

    Added documentation to Label and Rectangle

--- DanielFlappyGame/GameUtils/Label.cs
+++ DanielFlappyGame/GameUtils/Label.cs
@@ -8,18 +8,35 @@ using System.Text;
 
 namespace DanielFlappyGame.GameUtils
 {
+    /// <summary>
+    /// A util responsible for rendering a label into the Screen.
+    /// </summary>
     public class Label
     {
+        /// <summary>
+        /// The text of the label.
+        /// </summary>
         public string text;
-       
+       /// <summary>
+       /// The position of the label.
+       /// </summary>
         public Vector2 loc;
 
+        /// <summary>
+        /// Initiallize a label with given text and position.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="loc"></param>
         public Label(string text ,Vector2 loc)
         {
             this.text = text;           
             this.loc = loc;
         }
-
+        /// <summary>
+        /// Renders the Label to a given Screen with given TextRender
+        /// </summary>
+        /// <param name="screen">The screen to render to.</param>
+        /// <param name="render">The text render to render with.</param>
         public void RenderLabel(Screen screen  , TextRender render)
         {
             render.RenderText(screen , this.text , this.loc);
--- DanielFlappyGame/GameUtils/Rectangle.cs
+++ DanielFlappyGame/GameUtils/Rectangle.cs
@@ -6,13 +6,34 @@ using System.Text;
 
 namespace DanielFlappyGame.GameUtils
 {
+    /// <summary>
+    /// Holds the data of a 2D rectangle.
+    /// </summary>
     public class Rectangle
     {
+        /// <summary>
+        /// X axis of the rectangle origin.
+        /// </summary>
         private int x;
+        /// <summary>
+        /// Y axis of the rectangle origin.
+        /// </summary>
         private int y;
+        /// <summary>
+        /// The width of the Rectangle.
+        /// </summary>
         private int width;
+        /// <summary>
+        /// The height of the Rectangle.
+        /// </summary>
         private int height;        
-       
+        /// <summary>
+        /// Initiallizes a rectangle from given x coordinate, y coordinate , width and height.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
         public Rectangle(int x, int y, int width, int height)
         {
             this.x = x;
@@ -20,6 +41,12 @@ namespace DanielFlappyGame.GameUtils
             this.width = width;
             this.height = height;
         }
+        /// <summary>
+        /// Checks whether a point is inside the rectangle.
+        /// </summary>
+        /// <param name="x">The points X coordinate.</param>
+        /// <param name="y">The points Y coordinate.</param>
+        /// <returns></returns>
         public bool Contains(int x, int y)
         {
             return this.x <= x &&
@@ -36,7 +63,11 @@ namespace DanielFlappyGame.GameUtils
         }
 
 
-
+        /// <summary>
+        /// Checks wheter a Rectangle is inside the current Rectangle.
+        /// </summary>
+        /// <param name="rect">The Rectangle to check on.</param>
+        /// <returns></returns>
         public bool Contains(Rectangle rect)
         {
             return (this.x <= rect.x) &&
